b090a3fa6b05384dad78aba41dcd6e8fc9c2fb93
Sand Moshi <sandmoshi@gmail.com>
2021-10-13 00:59:37 -0400

    Added ESLint. Fixed breaking bug. Refactored code to be lint compliant.

--- src/defaultPopup.js
+++ src/defaultPopup.js
@@ -1,126 +1,124 @@
-const handleHoverSettingToggle = (toggleContainer, target) => {
-    const toggleInput = toggleContainer.children[0].firstElementChild;
-    if(event.target.tagName === 'P'){
-        toggleInput.checked = !toggleInput.checked
-    }
-    const checked = toggleInput.checked || false;
-    localStorage.setItem('hideWalletBalanceOnHover', JSON.stringify(checked));
-    // send message to zed.run tab (if active tab) to update blur
-    sendWalletBlurMessageToZedRun(checked);
-} 
+async function getCurrentTab() {
+  const queryOptions = { active: true, currentWindow: true };
+  const [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
 
 const sendWalletBlurMessageToZedRun = (blurEnabled) => {
-    getCurrentTab().then( currentTab => {
-        if(currentTab.url.indexOf('https://zed.run') > -1){
-            const currentTabId = currentTab.id;
-            chrome.tabs.sendMessage( currentTabId, {
-                message: 'walletBlur_changed',
-                blurEnabled: blurEnabled
-            })
-        }
-    })
-}
+  getCurrentTab().then((currentTab) => {
+    if (currentTab.url.indexOf('https://zed.run') > -1) {
+      const currentTabId = currentTab.id;
+      chrome.tabs.sendMessage(currentTabId, {
+        message: 'walletBlur_changed',
+        blurEnabled,
+      });
+    }
+  });
+};
+
+const handleHoverSettingToggle = (toggleContainer, target) => {
+  const toggleInput = toggleContainer.children[0].firstElementChild;
+  if (target.tagName === 'P') {
+    toggleInput.checked = !toggleInput.checked;
+  }
+  const checked = toggleInput.checked || false;
+  localStorage.setItem('hideWalletBalanceOnHover', JSON.stringify(checked));
+  // send message to zed.run tab (if active tab) to update blur
+  sendWalletBlurMessageToZedRun(checked);
+};
 
 const sendHighlightRaceMessageToZedRunCS = (args) => {
-    // highlightRacesByDistance_changed
-    getCurrentTab().then( currentTab => {
-        if(currentTab.url.indexOf('https://zed.run') > -1){
-            const currentTabId = currentTab.id;
-            const message = args.byDistance ? 'highlightRacesByDistance_changed' : args.byCost ? 'highlightRacesByCost_changed' : '';
-            const data = args.byDistance || args.byCost || null;
-            chrome.tabs.sendMessage( currentTabId, {
-                message,
-                data
-            })
-        }
-    })
-}
+  // highlightRacesByDistance_changed
+  getCurrentTab().then((currentTab) => {
+    if (currentTab.url.indexOf('https://zed.run') > -1) {
+      const currentTabId = currentTab.id;
+      const message = args.byDistance ? 'highlightRacesByDistance_changed' : args.byCost ? 'highlightRacesByCost_changed' : '';
+      const data = args.byDistance || args.byCost || null;
+      chrome.tabs.sendMessage(currentTabId, {
+        message,
+        data,
+      });
+    }
+  });
+};
 
 const hoverSettingToggleButton = document.getElementsByClassName('hoverSettingInput')[0];
 const hoverSettingToggleContainer = document.getElementsByClassName('hoverSettingContainer')[0];
 
 hoverSettingToggleContainer.addEventListener('click', (event) => {
-    handleHoverSettingToggle(event.currentTarget, event.target);
+  handleHoverSettingToggle(event.currentTarget, event.target);
 });
 
-function loadPreviousSettings(){
-    console.log('load previous settings');
-    const hideWalletBalance = JSON.parse(localStorage.getItem('hideWalletBalanceOnHover')) || false;
-    hoverSettingToggleButton.checked = hideWalletBalance;
-    sendWalletBlurMessageToZedRun(hideWalletBalance);
-    const highlightRaceByDistanceSavedData = JSON.parse(localStorage.getItem('highlightedRacesByDistance')) || false;
-    highlightRaceByDistance.updateUi(highlightRaceByDistanceSavedData);
-    sendHighlightRaceMessageToZedRunCS({byDistance: highlightRaceByDistanceSavedData})
-}
-
-window.onload = function() {
-    loadPreviousSettings();
-};
-
-async function getCurrentTab() {
-    let queryOptions = { active: true, currentWindow: true };
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab;
-}
-
-function localizeStrings(){
-    document.querySelectorAll('[data-locale]').forEach(elem => {
-        elem.innerText = chrome.i18n.getMessage(elem.dataset.locale)
-    })
+function localizeStrings() {
+  document.querySelectorAll('[data-locale]').forEach((elem) => {
+    elem.innerText = chrome.i18n.getMessage(elem.dataset.locale);
+  });
 }
 
 localizeStrings();
 
-class highlightRaceByDistanceClass {
-    constructor(){
-        this.distances = ['1000m', '1200m', '1400m','1600m','1800m','2000m','2200m','2400m']
-        this.data = {
-            '1000m': false,
-            '1200m': false, 
-            '1400m': false,
-            '1600m': false,
-            '1800m': false,
-            '2000m': false,
-            '2200m': false,
-            '2400m': false,
-        };
+const HighlightRaceByDistance = function () {
+  this.distances = ['1000m', '1200m', '1400m', '1600m', '1800m', '2000m', '2200m', '2400m'];
+  this.data = {
+    '1000m': false,
+    '1200m': false,
+    '1400m': false,
+    '1600m': false,
+    '1800m': false,
+    '2000m': false,
+    '2200m': false,
+    '2400m': false,
+  };
+  this.toggleButton = (element) => {
+    const distance = element && element.innerText;
+    const newValue = !this.data[distance];
+    if (distance) { this.data[distance] = newValue; }
+    localStorage.setItem('highlightedRacesByDistance', JSON.stringify(this.data));
+    this.updateSingleButton(element, newValue);
+    sendHighlightRaceMessageToZedRunCS({ byDistance: this.data });
+  };
+  this.updateSingleButton = (element, newValue) => {
+    if (element) {
+      newValue ? element.classList.add('on') : element.classList.remove('on');
     }
-    toggleButton = (element) => {
-        const distance = element && element.innerText;
-        let newValue = !this.data[distance];
-        if(distance){ this.data[distance] = newValue};
-        localStorage.setItem('highlightedRacesByDistance', JSON.stringify(this.data));
-        this.updateSingleButton(element, newValue);
-        sendHighlightRaceMessageToZedRunCS({byDistance: this.data})
-    };
-    updateSingleButton = (element, newValue) => {
-        if(element){
-            newValue ? element.classList.add('on'): element.classList.remove('on')
-        }
-    };
-    updateUi = (highlightRaceByDistanceSavedData) => {
-        if(!highlightRaceByDistanceSavedData){ return }
-        this.data = highlightRaceByDistanceSavedData;
-        this.distances.forEach( (distance, index) => {
-            const container = document.getElementById('ditanceToggleBtnGroup');
-            const element = container.getElementsByClassName('distanceToggleBtn')[index];
-            if(element.innerText !== distance){
-                throw Error('did not select correct item')
-            }
-            // If enabled for this distance, change appearance of the button
-            if(highlightRaceByDistanceSavedData[distance] == true){
-                element.classList.add('on');
-            }
-        })
-    };
-}
+  };
+  this.updateUi = (highlightRaceByDistanceSavedData) => {
+    if (!highlightRaceByDistanceSavedData) { return; }
+    this.data = highlightRaceByDistanceSavedData;
+    this.distances.forEach((distance, index) => {
+      const container = document.getElementById('ditanceToggleBtnGroup');
+      const element = container.getElementsByClassName('distanceToggleBtn')[index];
+      if (element.innerText !== distance) {
+        throw Error('did not select correct item');
+      }
+      // If enabled for this distance, change appearance of the button
+      if (highlightRaceByDistanceSavedData[distance] == true) {
+        element.classList.add('on');
+      }
+    });
+  };
+};
 
 // Initialize class
-const highlightRaceByDistance = new highlightRaceByDistanceClass();
+const highlightRaceByDistance = new HighlightRaceByDistance();
+
+function loadPreviousSettings() {
+  console.log('load previous settings');
+  const hideWalletBalance = JSON.parse(localStorage.getItem('hideWalletBalanceOnHover')) || false;
+  hoverSettingToggleButton.checked = hideWalletBalance;
+  sendWalletBlurMessageToZedRun(hideWalletBalance);
+  const highlightRaceByDistanceSavedData = JSON.parse(localStorage.getItem('highlightedRacesByDistance')) || false;
+  highlightRaceByDistance.updateUi(highlightRaceByDistanceSavedData);
+  sendHighlightRaceMessageToZedRunCS({ byDistance: highlightRaceByDistanceSavedData });
+}
+
+window.onload = () => {
+  loadPreviousSettings();
+};
 
 const highlightRacesByDistanceButtons = document.getElementsByClassName('distanceToggleBtn');
-for( let button of highlightRacesByDistanceButtons){s
-    button.addEventListener('click', () => {
-        highlightRaceByDistance.toggleButton(button);
-    })
-};
+for (const button of highlightRacesByDistanceButtons) {
+  button.addEventListener('click', () => {
+    highlightRaceByDistance.toggleButton(button);
+  });
+}
--- src/zedRunPage.js
+++ src/zedRunPage.js
@@ -1,111 +1,111 @@
 // Meant to run on Zed.Run Pages
-const showFatigue = () => {
-    const intervalID = setInterval(() => {
-        const rectElements = document.getElementsByTagName('rect');
-        if(rectElements.length > 0){
-            clearInterval(intervalID);
-            addFatigueValueToDom(rectElements);
-        }
-    }, 500)
-}
 
 const addFatigueValueToDom = (rectElements) => {
-    const element = rectElements[0];
-    const fillAttribue = element.getAttribute('fill');
-    const fatigueValue = fillAttribue.length === 20 ? fillAttribue.substring(16,19) : fillAttribue.substring(16,18)
-    const fatigueText = document.createElement('h1');
-    fatigueText.innerText = fatigueValue;
-    fatigueText.style.color = 'white';
-    const fatigueContainer = document.getElementsByClassName('fatigue-meter-lg')[0];
-    fatigueContainer.appendChild(fatigueText);
-}
+  const element = rectElements[0];
+  const fillAttribue = element.getAttribute('fill');
+  const fatigueValue = fillAttribue.length === 20 ? fillAttribue.substring(16, 19) : fillAttribue.substring(16, 18);
+  const fatigueText = document.createElement('h1');
+  fatigueText.innerText = fatigueValue;
+  fatigueText.style.color = 'white';
+  const fatigueContainer = document.getElementsByClassName('fatigue-meter-lg')[0];
+  fatigueContainer.appendChild(fatigueText);
+};
+
+const showFatigue = () => {
+  const intervalID = setInterval(() => {
+    const rectElements = document.getElementsByTagName('rect');
+    if (rectElements.length > 0) {
+      clearInterval(intervalID);
+      addFatigueValueToDom(rectElements);
+    }
+  }, 500);
+};
 
 const updateWalletBlur = (blurEnabled) => {
-    const root = document.querySelector(':root');
-    console.log('blurEnabled', blurEnabled);
-    localStorage.setItem('hideWalletBalanceOnHover', JSON.stringify(blurEnabled));
-    if(blurEnabled){
-        root.style.setProperty('--walletBlur', '3px');
-    }
-    else{
-        root.style.setProperty('--walletBlur', '0px');
-    }
-}
+  const root = document.querySelector(':root');
+  console.log('blurEnabled', blurEnabled);
+  localStorage.setItem('hideWalletBalanceOnHover', JSON.stringify(blurEnabled));
+  if (blurEnabled) {
+    root.style.setProperty('--walletBlur', '3px');
+  } else {
+    root.style.setProperty('--walletBlur', '0px');
+  }
+};
 
 const updateHighlightRaces = (args) => {
-    if(args.byDistance){
-        localStorage.setItem('highlightedRacesByDistance', JSON.stringify(args.byDistance))
-        highlightRaces({byDistance: args.byDistance})
-    }
-}
+  if (args.byDistance) {
+    localStorage.setItem('highlightedRacesByDistance', JSON.stringify(args.byDistance));
+    highlightRaces({ byDistance: args.byDistance });
+  }
+};
 
 const highlightRaces = (args) => {
-    if(args.byDistance){
-        const raceRows = document.getElementsByClassName('panel');
-        for(let race of raceRows){
-            const raceDistance = race.getElementsByClassName('distance')[0].innerText;
-            // Check if this distance is to be highlighted
-            if(args.byDistance[raceDistance]){
-                race.classList.add('highlightRaceRow');
-            }else{
-                race.classList.remove('highlightRaceRow');
-            }
-        }
+  if (args.byDistance) {
+    const raceRows = document.getElementsByClassName('panel');
+    for (const race of raceRows) {
+      const raceDistance = race.getElementsByClassName('distance')[0].innerText;
+      // Check if this distance is to be highlighted
+      if (args.byDistance[raceDistance]) {
+        race.classList.add('highlightRaceRow');
+      } else {
+        race.classList.remove('highlightRaceRow');
+      }
     }
-}   
-
-const loadPreviousSettings = () => {
-    loadPreviousWalletBlurSetting();
-}
+  }
+};
 
 const loadPreviousWalletBlurSetting = () => {
-    const hideWalletBalance = JSON.parse(localStorage.getItem('hideWalletBalanceOnHover')) || false;
-    updateWalletBlur(hideWalletBalance);
+  const hideWalletBalance = JSON.parse(localStorage.getItem('hideWalletBalanceOnHover')) || false;
+  updateWalletBlur(hideWalletBalance);
+};
+
+const loadPreviousSettings = () => {
+  loadPreviousWalletBlurSetting();
 };
 
 const loadHightlightRaceByDistanceSettingsandRace = () => {
-    console.log('loadHightlightRaceByDistanceSettingsandRace()');
-    const highlightRaceByDistanceSavedData = JSON.parse(localStorage.getItem('highlightedRacesByDistance')) || false;
-    highlightRaces({byDistance: highlightRaceByDistanceSavedData});
-}
+  console.log('loadHightlightRaceByDistanceSettingsandRace()');
+  const highlightRaceByDistanceSavedData = JSON.parse(localStorage.getItem('highlightedRacesByDistance')) || false;
+  highlightRaces({ byDistance: highlightRaceByDistanceSavedData });
+};
 
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        console.log('message recevied', request.message)
-        if(request.message === 'url_changed'){
-            onContentScriptLoad();
-        }
-        if(request.message === 'walletBlur_changed'){
-            updateWalletBlur(request.blurEnabled || false);
-        }
-        if(request.message === 'highlightRacesByDistance_changed'){
-            updateHighlightRaces({byDistance: request.data});
-        }
+  (request, sender, sendResponse) => {
+    console.log('message recevied', request.message);
+    if (request.message === 'url_changed') {
+      onContentScriptLoad();
     }
-)
+    if (request.message === 'walletBlur_changed') {
+      updateWalletBlur(request.blurEnabled || false);
+    }
+    if (request.message === 'highlightRacesByDistance_changed') {
+      updateHighlightRaces({ byDistance: request.data });
+    }
+  },
+);
 
-function onContentScriptLoad(){
-    // Runs on Zed Run page load, and enables features depending on page
-    loadPreviousWalletBlurSetting();
-    loadPreviousSettings();
-    if(location.href.indexOf('https://zed.run/racehorse/') > -1){
-        showFatigue();
-    }
-    if(location.href.indexOf('https://zed.run/racing/events') > -1 ){
-        loadHightlightRaceByDistanceSettingsandRace();
-        // Listen to changes in DOM
-        const targetNode = document.getElementsByClassName('accordion')[0];
-        const raceMutationObserver = ( (mutationList, observerInstance) => {
-            // Try to get the rows
-            for(const mutation of mutationList){
-                if(mutation.type === 'childList' || mutation.type === 'subtree'){
-                    loadHightlightRaceByDistanceSettingsandRace();
-                }
-            }
-        })
-        const observer = new MutationObserver(raceMutationObserver);
-        targetNode && observer.observe(targetNode, { childList:true, attributes: false, subtree: true })
-    }
+function onContentScriptLoad() {
+  // Runs on Zed Run page load, and enables features depending on page
+  loadPreviousWalletBlurSetting();
+  loadPreviousSettings();
+  if (location.href.indexOf('https://zed.run/racehorse/') > -1) {
+    showFatigue();
+  }
+  if (location.href.indexOf('https://zed.run/racing/events') > -1) {
+    loadHightlightRaceByDistanceSettingsandRace();
+    // Listen to changes in DOM
+    const targetNode = document.getElementsByClassName('accordion')[0];
+    const raceMutationObserver = ((mutationList, observerInstance) => {
+      // Try to get the rows
+      for (const mutation of mutationList) {
+        if (mutation.type === 'childList' || mutation.type === 'subtree') {
+          loadHightlightRaceByDistanceSettingsandRace();
+        }
+      }
+    });
+    const observer = new MutationObserver(raceMutationObserver);
+    targetNode && observer.observe(targetNode, { childList: true, attributes: false, subtree: true });
+  }
 }
 
 // Run these functions immediately
